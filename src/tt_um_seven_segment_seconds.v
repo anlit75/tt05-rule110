e0d0eb090b956a7cf7a312863d8cfd37414228cc
anson40512 <anson40512@gmail.ccom>
2023-10-07 13:06:17 +0800

    Rule110 for tt05

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -1,60 +1,93 @@
 `default_nettype none
 
-module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+module tt_um_rule110_an(
+  	input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 output pin
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
-);
+); 
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+  	// Set all bidirectional pins as outputs
+    assign uio_oe = 8'b1;
+  
+ 	 // Initialize unused inputs of the BIDIRECTIONAL path to 0 for posterity (otherwise Yosys fails)
+    assign uio_in = 8'b0;
+  
+//   	wire [15:0] data, left, right;
+//   	reg [15:0] q;
 
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
+//     assign data = 16'b0000000000000001;  
+//     assign left = {q[0], q[15:1]};
+//     assign right = {q[14:0], q[15]};
+  
+//   	assign {uo_out, uio_out} = q;
+        
+//     always @(posedge clk) begin
+//       if (!rst_n) q <= data;
+//         else q <= (left & q & ~right) | (~left & q) | (~q & right);
+//     end
+  
+  parameter LOAD=0;	// load data
+  parameter STEP=1;	// step into next generation
+  parameter S0=2, S15=17;	// 16 cycles output, each cycle output 16 bits
+  parameter UPDATE=18;		// update rule_in
+  
+  reg [4:0] state, next_state;
+  reg [255:0] rule_in, rule_out;
+  
+  always @(posedge clk) begin
+    if (!rst_n) state <= LOAD;
+    else state <= next_state;
+  end
+  
+  always @(*) begin
+    case (state) 
+      LOAD: next_state = STEP;
+      STEP: next_state = S0;
+      UPDATE: next_state = STEP;
+      default: next_state = next_state + 1;
+    endcase
+  end
+  
+  always @(*) begin
+    if (!rst_n) rule_in = 256'b0;
+//     else if (state == LOAD) rule_in = 256'b10001110111000100110111110001001101111100010011011111000100;
+    else if (state == LOAD) rule_in = ui_in;
+    else if (state == UPDATE) rule_in = rule_out;
+    else rule_in = rule_in;
+  end
+  
+  assign {uo_out, uio_out} = (state >= S0 && state <= S15) ? rule_out[16*(18-state)-1 -: 16] : {uo_out, uio_out};
+  
+  rule110 rule110(
+    .in(rule_in),
+    .out(rule_out),
+    .ena(state == STEP),
+    .clk(clk),
+    .rst(!rst_n)
+  );
+  
+endmodule
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
 
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
+module rule110(
+    input  wire [255:0] in,
+    output wire [255:0] out,
+    
+    input  wire 	  ena,  
+    input  wire       clk,
+    input  wire       rst
+); 
 
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
+    wire [255:0] left, right;
 
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+    assign left =  {1'b0, in[255:1]};
+    assign right = {in[254:0], 1'b0};
+        
+  	assign out = ena ? (left & in & ~right) | (~left & in) | (~in & right) : out;
 endmodule
