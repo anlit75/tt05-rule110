34d4c7f0d0b19c69c475ac80082aae52df0a6197
anson40512 <anson40512@gmail.ccom>
2023-10-07 15:38:56 +0800

    fix logic loop

--- src/tt_um_rule110_an.v
+++ src/tt_um_rule110_an.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_rule110(
+module tt_um_rule110_an(
   	input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 output pin
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
